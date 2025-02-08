ca9ddc49ab64412831aa1a059b6ca3c64980d45e
Jimmy Liu <jimmyliu20040@gmail.com>
2025-02-08 09:58:07 -0800

    Adding header and new icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 function App() {
 
   return (
-    <div className='flex flex-col p-4'>
+    <div className='flex flex-col p-4 max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
-        <header>
-          <h1><span className='text-blue-400'>Echo</span></h1>
+        <header className='flex items-center justify-between gap-4'>
+          <h1 className='text-blue-400'>Echo</h1>
+          <button className='flex items-center gap-2'>
+            <p>New</p>
+            <i className="fa-solid fa-circle-plus"></i>
+          </button>
         </header>
         <main className='flex-1'>
 
