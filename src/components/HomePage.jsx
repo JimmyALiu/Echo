63c698da4cad40f2cd708ec0170ab39cc8681c25
Jimmy Liu <jimmyliu20040@gmail.com>
2025-02-13 16:53:09 -0800

    Implementing startRecording function
    Function for recording user input

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,8 +7,40 @@ export default function HomePage(props) {
     const [audioChunks, setAudioChunks] = useState([])
     const [duration, setDuration] = useState(0)
 
-    const mediaRecorder = useRef(null)
+    const mediaRecorder = useRef(null)  // useref used is a current object that does not trigger a re-render
+    const mimeType = 'audio/webm'
 
+    async function startRecording() {
+        let tempStream
+
+        console.log("Recording started")
+
+        try {
+            const streamData = navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: false
+            })
+            tempStream = streamData
+
+        } catch(err) {
+            console.log(err)
+            return
+        }
+
+        const media = new MediaRecorder(tempStream, { type: mimeType })
+
+        mediaRecorder.current = media
+        mediaRecorder.current.start()
+        
+        let localAudioChunks = []
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === 'undefined') return
+            if (event.data.size === 0) return
+            localAudioChunks.push(event.data)
+        }
+        setAudioChunks(localAudioChunks)
+    }
+    
     return (
         <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20'>
             <h1 className='text-blue-400 bold font-semibold text-5xl sm: text-6xl md:text-7xl'>Echo</h1>
