ec9d98fbe1f8d18799f1307328d13468feb990b3
Jimmy Liu <jimmyliu20040@gmail.com>
2025-02-16 00:20:02 -0800

    Styling updates

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -4,7 +4,7 @@ export default function FileDisplay(props) {
     const { audioStream, file, handleAudioReset } = props
 
     return (
-        <main className='flex-1 p-4 flex flex-col gap-3 w-fit max-w-full mx-auto text-center sm:gap-4 justify-center pb-20'>
+        <main className='flex-1 p-4 flex flex-col gap-3 w-82 max-w-full mx-auto text-center sm:w-96 sm:gap-4 justify-center pb-20'>
             <h1 className='bold font-semibold text-4xl sm: text-5xl md:text-6xl'>Your <span className='text-blue-400 bold'>File</span></h1>
 
             <div className='mx-auto my-4 flex flex-col text-left'>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className='flex items-center justify-between p-4 gap-4'>
             <h1 className='text-blue-400 bold font-medium text-xl'>Echo</h1>
-            <button className='flex items-center gap-2 specialBtn px-4 py-2 rounded-lg text-blue-400'>
+            <button className='flex items-center gap-2 specialBtn px-4 py-2 rounded-lg text-sm text-blue-400'>
                 <p>New</p>
                 <i className="fa-solid fa-circle-plus"></i>
             </button>
