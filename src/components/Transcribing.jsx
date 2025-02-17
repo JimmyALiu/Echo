813e0047913ec5b2069d78a066197b0d47c83459
Jimmy Liu <jimmyliu20040@gmail.com>
2025-02-16 19:24:49 -0800

    Setting up the transcribing page

---
+++ src/components/Transcribing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Transcribing() {
+  return (
+    <div className='flex items-center flex-col justify-center gap-10 md:gap-14 py-24'>
+        <div className='flex flex-col gap-2 sm:gap-4'>
+            <h1 className='bold font-semibold text-4xl sm: text-5xl md:text-6xl'><span className='text-blue-400 bold'>Transcribing</span></h1>
+
+            <p>{!downloading ? 'warming up cylinders' : 'processing...'}</p>
+
+        </div>
+
+        <div className='flex flex-col gap-2 sm:Gap-4 max-w-[500px] mx-auto w-full'>
+            
+        </div>
+    </div>
+  )
+}
