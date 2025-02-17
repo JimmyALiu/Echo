41d234253009eda5eb1cb9deab7b1b5ab845862f
Jimmy Liu <jimmyliu20040@gmail.com>
2025-02-16 19:32:56 -0800

    Setting up page display logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react'
 import FileDisplay from './components/FileDisplay'
 import HomePage from './components/HomePage'
 import Header from './components/Header'
+import Information from './components/Information'
 
 
 function App() {
     const [file, setFile] = useState(null)
     const [audioStream, setAudioStream] = useState(null)
+    const [output, setOutput] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     const isAudioAvailable = file || audioStream
 
@@ -19,9 +22,15 @@ function App() {
         <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
             <section className='min-h-screen flex flex-col'>
                 <Header />
-                {isAudioAvailable ?
-                    <FileDisplay audioStream={audioStream} file={file} handleAudioReset={handleAudioReset} /> :
-                    <HomePage setAudioStream={setAudioStream} setFile={setFile} />}
+                {output ? (
+                    <Information />
+                ) : loading ? (
+                    <Transcribing />
+                ) : isAudioAvailable ? (
+                    <FileDisplay audioStream={audioStream} file={file} handleAudioReset={handleAudioReset} />
+                ) : (
+                    <HomePage setAudioStream={setAudioStream} setFile={setFile} />
+                )}
             </section>
             <footer></footer>
         </div>
