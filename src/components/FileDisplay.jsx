7e586bbd736e5a5d946878882c48dd16ab1a18b9
Jimmy Liu <jimmyliu20040@gmail.com>
2025-02-24 00:03:47 -0800

    Styling transcription header

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -5,7 +5,7 @@ export default function FileDisplay(props) {
 
     return (
         <main className='flex-1 p-4 flex flex-col gap-3 w-82 max-w-full mx-auto text-center sm:w-96 sm:gap-4 justify-center pb-20'>
-            <h1 className='bold font-semibold text-4xl sm: text-5xl md:text-6xl'>Your <span className='text-blue-400 bold'>File</span></h1>
+            <h1 className='bold font-semibold text-4xl sm:text-5xl text-5xl md:text-6xl'>Your <span className='text-blue-400 bold'>File</span></h1>
 
             <div className='mx-auto my-4 flex flex-col text-left'>
                 <h3 className='font-semibold'>Name</h3>
--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Information() {
   return (
-    <div>Information</div>
+    <main className='flex-1 p-4 flex flex-col gap-3 max-w-full mx-auto text-center max-w-prose w-full sm:gap-4 justify-center pb-20'>
+            <h1 className='bold font-semibold text-4xl sm:text-5xl text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-blue-400 bold'>Transcription</span></h1>
+
+            
+        </main>
   )
 }
