43223f7ac5de4290f422ebdc86ea054b381252b0
Jimmy Liu <jimmyliu20040@gmail.com>
2025-02-25 23:43:20 -0800

    Styling tab system

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -7,7 +7,7 @@ export default function Information(props) {
         <main className='flex-1 p-4 flex flex-col gap-3 max-w-full mx-auto text-center max-w-prose w-full sm:gap-4 justify-center pb-20'>
             <h1 className='bold font-semibold text-4xl sm:text-5xl text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-blue-400 bold'>Transcription</span></h1>
 
-            <div className='flex mx-auto bg-white border-solid border-blue-300 shadow rounded-full overflow-hidden items-center gap-2'>
+            <div className='grid grid-cols-2 mx-auto bg-white border-solid border-blue-300 shadow rounded-full overflow-hidden items-center'>
                 <button onClick={() => setTab('transcription') } className={'px-4 py-1 font-medium duration-200 ' + (tab === 'transcription' ? 'bg-blue-400 text-white hover:text-gray-100' : 'text-blue-400 hover:text-blue-600')}>Transcription</button>
                 <button onClick={() => setTab('translation') } className={'px-4 py-1 font-medium duration-200 ' + (tab === 'translation' ? 'bg-blue-400 text-white hover:text-gray-100' : 'text-blue-400 hover:text-blue-600')}>Translation</button>
             </div>
