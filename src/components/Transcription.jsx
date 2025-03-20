4e567daa4207c0688314f4f8955aaa2f35bea435
Jimmy Liu <jimmyliu20040@gmail.com>
2025-03-20 13:29:55 -0700

    Cleaning up code

--- src/components/Transcription.jsx
+++ src/components/Transcription.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 export default function Transcription(props) {
     const { output } = props
     const finalText = output.map(val => val.text)
-    console.log(output)
-    console.log(typeof output)
 
     return (
-        <div>{output[0].text}</div>
+        <div>{finalText}</div>
     )
 }
