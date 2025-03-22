34a67a595efaf14721f02bc4e7b514236d2e5b3b
Jimmy Liu <jimmyliu20040@gmail.com>
2025-03-21 21:52:33 -0700

    conditionally render translation

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -6,7 +6,16 @@ export default function Translation(props) {
 
     return (
         <div className='flex flex-col gap-2 max-w-[400px] w-full mx-auto'>
-            <div className='flex items-stretch gap-2'>
+
+            {translating ? 
+                <>
+                    <p>{translation}</p>
+                    <div className='grid place-items-center'>
+
+                    </div>
+                </>
+            : 
+            (<div className='flex items-stretch gap-2'>
                 <select className='flex-1 outline-none bg-white focus:outline-none border border-transparent p-2 rounded-[10px] hover:border-blue-300 duration-200' value={toLanguage} onChange={(e) => setToLanguage(e.target.value)}>
                     <option value={'Select language'}>Select language</option>
                     {Object.entries(LANGUAGES).map(([key, value]) => {
@@ -17,7 +26,7 @@ export default function Translation(props) {
                 </select>
 
                 <button className='specialBtn px-3 py-2 text-blue-400 rounded-[10px] cursor-pointer hover:text-blue-600 duration-200'>Translate</button>
-            </div>
+            </div>)}
         </div>
     )
 }
