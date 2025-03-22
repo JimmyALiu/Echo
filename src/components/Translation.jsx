cf217c0a8de599674521f43c2aa595e16aa8d359
Jimmy Liu <jimmyliu20040@gmail.com>
2025-03-21 21:30:04 -0700

    Bugfix
    selection not updating visually

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -8,7 +8,7 @@ export default function Translation(props) {
         <div className='flex flex-col gap-2 max-w-[400px] w-full mx-auto'>
             <div className='flex items-stretch gap-2'>
                 <select className='flex-1 outline-none bg-white focus:outline-none border border-transparent p-2 rounded-[10px] hover:border-blue-300 duration-200' value={toLanguage} onChange={(e) => setToLanguage(e.target.value)}>
-                    <option value={toLanguage}>Select language</option>
+                    <option value={'Select language'}>Select language</option>
                     {Object.entries(LANGUAGES).map(([key, value]) => {
                         return (
                             <option key={key} value={value}>{key}</option>
